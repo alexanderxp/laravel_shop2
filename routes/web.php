c9c22ab03ea733b9a5b89f43b983175895c5bd2f
Alexander <alexanderxp@ukr.net>
2021-10-02 04:42:59 +0300

    add product.blade.php

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/categories', function () {   
     return view('categories');              
 }); 
+
+Route::get('/mobiles/iphone_x_64', function () {
+    return view('product');                       
+});
