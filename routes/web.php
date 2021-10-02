5d9e88c292ec4f987a5e21c5aeaaa07b78155b4b
Alexander <alexanderxp@ukr.net>
2021-10-02 12:10:02 +0300

    add controller

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('index');              
 });           
 
+Route::get('/', 'MainController@index');
+
 Route::get('/categories', function () {   
     return view('categories');              
 }); 
