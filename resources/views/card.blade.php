717daf06e49ae0043e5fb19bf06c241b7dc98300
Alexander <alexanderxp@ukr.net>
2021-10-05 01:43:18 +0300

    add  @isset($category) {{ $category->name }} @endisset      in  card.blade.php

--- resources/views/card.blade.php
+++ resources/views/card.blade.php
@@ -9,10 +9,13 @@
             <h3>iPhone X 64GB</h3>
             <p>71990 ?</p>
             <p>
-                <form action="{{route('basket')}}" method="POST">   
+                <form action="{{route('basket')}}" method="POST">
                     <button type="submit" class="btn btn-primary" role="button">
                         В корзину
                     </button>
+                    @isset($category)                
+                    {{ $category->name }}          
+                    @endisset               
                     <a href="http://internet-shop.tmweb.ru/mobiles/iphone_x_64"
                         class="btn btn-default"
                         role="button">Подробнее
