a929918b0052281afea72659d0098ad65d5b055a
Alexander <alexanderxp@ukr.net>
2021-10-04 22:16:42 +0300

    add    $code    $category = Category::where('code', $code)->first();     return view('category', compact('category'));    to   MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
@@ -12,8 +13,9 @@ class MainController extends Controller
     public function categories(){  
         return view('categories');          
     }    
-    public function category($category){     
-        return view('category',compact('category'));  
+    public function category($code){                           
+        $category = Category::where('code', $code)->first(); 
+        return view('category', compact('category'));
     }       
     public function product($product =null){  
         return view('product' ,['product'=> $product]);  
