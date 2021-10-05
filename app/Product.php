cd91c60ac804b6fa62aa2d653da35d6144307d23
Alexander <alexanderxp@ukr.net>
2021-10-05 18:18:26 +0300

    add public function getCategory()  to Product.php

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function getCategory()
+    {
+        $category = Category::where('id', this ->category_id)->get();
+        dd($category);
+    }
 }
