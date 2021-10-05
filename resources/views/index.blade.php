339c376bb0ec1d266327fc0b8f304e7d3cd77a89
Alexander <alexanderxp@ukr.net>
2021-10-05 03:37:50 +0300

    fix mistake   @foreach($products as $product) @include('card', compact('product'))  @endforeach   in  index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,14 @@
 @section('title', 'Главная')
 
 @section ('content')
+
 <div class="starter-template">
     <h1>Все товары</h1>
 
     <div class="row">
-        @include('card')
+        @foreach($products as $product)
+            @include('card', compact('product'))
+        @endforeach                  
     </div>
 </div>
 
