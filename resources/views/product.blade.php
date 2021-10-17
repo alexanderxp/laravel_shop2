32aad1cf27a6665ab05f09a54ac88f7091929aaa
Alexander <alexanderxp@ukr.net>
2021-10-18 00:10:25 +0300

    fix mistakes [file -> 'product'] to ['file' => 'product'] & del <h2>Мобильные телефоны</h2> in product.blade.php

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,4 +1,4 @@
-@extends('master', [file -> 'product'] )
+@extends('master', ['file' => 'product'] )
 
 @section('title' , 'Товар') 
 
@@ -7,8 +7,7 @@
     <div class="starter-template">
                             <h1>iPhone X 64GB</h1>
                             <h2>{{ $product }}</h2>
-    <h2>Мобильные телефоны</h2>
-    <p>Цена: <b>71990 ?</b></p>
+    <p>Цена: <b>71990 $</b></p>
     <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg">
     <p>Отличный продвинутый телефон с памятью на 64 gb</p>
 
