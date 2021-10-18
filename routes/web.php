9ccb63cf0f4431dbf61d935f807d30900bf62ae2
Alexander <alexanderxp@ukr.net>
2021-10-18 11:45:50 +0300

    add Route::post('/basket/remove/{id}', 'BasketController@basketRemove')->name('basket-remove'); to web.php

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::get('/basket/place', 'BasketController@basketPlace')-> name('basket-place
 
 Route::post('/basket/add/{id}', 'BasketController@basketAdd')->name('basket-add');
 
+Route::post('/basket/remove/{id}', 'BasketController@basketRemove')->name('basket-remove');
+
 Route::get('/{category}', 'MainController@category')-> name('category');       
 
 Route::get('/{category}/{product?}', 'MainController@product')-> name('product');
