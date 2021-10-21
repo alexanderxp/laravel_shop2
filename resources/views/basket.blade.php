bb152a789ce1d01cad534c17f080ce2fb6d6b446
Alexander <alexanderxp@ukr.net>
2021-10-21 11:22:56 +0300

    change <td>{{ $product->getPriceForCount($product->pivot->count) }} $</td>    to  <td>{{ $product->getPriceForCount() }} $</td>   in basket.blade.php

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -56,7 +56,7 @@
                     </div>                                             
                 </td>                                                    
                 <td>{{ $product->price }} $</td>
-                <td>{{ $product->getPriceForCount($product->pivot->count) }} $</td>                                                                               
+                <td>{{ $product->getPriceForCount() }} $</td>                                                                               
             </tr>
             @endforeach                                                         
             <tr>                                                           
