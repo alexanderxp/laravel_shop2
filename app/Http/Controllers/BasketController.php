86fbcbc127fb8e9ee7656986307039ac300b74ed
Alexander <alexanderxp@ukr.net>
2022-01-05 00:55:04 +0200

    add (Request $request) & $orderId = session ( 'orderId' );if... $order = Order::find($orderId);dd($request->all());return redirect()->route('index');  to  BasketController.php

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -14,9 +14,15 @@ class BasketController extends Controller
         }
         return view('basket', compact('order'));
     }
-    public function basketConfirm(){
-
-    }
+    public function basketConfirm(Request $request){
+        $orderId = session ( 'orderId' );                
+        if (is_null($orderId)){                         
+           return redirect()->route( 'index' );               
+        }                                                
+        $order = Order::find($orderId);                 
+        dd($request->all());
+        return redirect()->route('index');         
+     }
     public function basketPlace(){
         $orderId = session ( 'orderId' );            
         if (is_null($orderId)){                       
