0be436e98e676e44ce5176478e6545d2deb391f3
Alexander <alexanderxp@ukr.net>
2022-01-07 11:13:47 +0200

    add @yield('title') & @yield('title') del <form...</form> add @yield('comtent') in master.blade.php

--- resources/views/auth/layouts/master.blade.php
+++ resources/views/auth/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Админка: Регистрация</title>
+    <title>Админка: @yield('title')</title>
 
     <!-- Scripts -->
     <script src="/js/app.js" defer></script>
@@ -49,57 +49,10 @@
             <div class="row justify-content-center">
                     <div class="col-md-8">
         <div class="card">
-            <div class="card-header">Регистрация</div>
-            <form method="POST" action="http://internet-shop.tmweb.ru/register" aria-label="Register">
-                <input type="hidden" name="_token" value="ly1JLlbqtZnTctk5xSOikUchaLf9zdXceSpYj7BV">                <div class="form-group row">
-                    <label for="name" class="col-md-4 col-form-label text-md-right">Имя</label>
+        <div class="card-header">@yield('title')</div>
 
-                    <div class="col-md-6">
-                        <input id="name" type="text" class="form-control" name="name"
-                               value="" required autofocus>
-
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail</label>
-
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control"
-                               name="email" value="" required>
-
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label for="password" class="col-md-4 col-form-label text-md-right">Пароль</label>
-
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control"
-                               name="password" required>
-
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Подтвердите
-                        пароль</label>
-
-                    <div class="col-md-6">
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation"
-                               required>
-                    </div>
-                </div>
-
-                <div class="form-group row mb-0">
-                    <div class="col-md-6 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
-                            Зарегистрироваться
-                        </button>
-                    </div>
-                </div>
-            </form>
             <div class="card-body">
+            @yield('comtent')
             </div>
         </div>
     </div>
