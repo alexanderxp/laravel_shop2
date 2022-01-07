80c945c992b762b9543c83e3b779973384610188
Alexander <alexanderxp@ukr.net>
2022-01-07 14:23:00 +0200

    add {{ route('register') }} & @csrf  to register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,39 +3,39 @@
 @section('title', 'Регистрация')
 
 @section('content')
-<form method="POST" action="http://internet-shop.tmweb.ru/register" aria-label="Register">
-    <input type="hidden" name="_token" value="ly1JLlbqtZnTctk5xSOikUchaLf9zdXceSpYj7BV">                
-        <div class="form-group row">
-            <label for="name" class="col-md-4 col-form-label text-md-right">
-                Имя
-            </label>
-            <div class="col-md-6">
-                <input id="name" type="text" class="form-control" name="name"
-                    value="" required autofocus>
-            </div>
+<form method="POST" action="{{ route('register') }}" aria-label="Register">
+    @csrf                  
+    <div class="form-group row">
+        <label for="name" class="col-md-4 col-form-label text-md-right">
+            Имя
+        </label>
+        <div class="col-md-6">
+            <input id="name" type="text" class="form-control" name="name"
+                value="" required autofocus>
         </div>
-        <div class="form-group row">
-            <label for="email" class="col-md-4 col-form-label text-md-right">
-                E-Mail
-            </label>
-            <div class="col-md-6">
-                <input id="email" type="email" class="form-control"
-                    name="email" value="" required>
-            </div>
+    </div>
+    <div class="form-group row">
+        <label for="email" class="col-md-4 col-form-label text-md-right">
+            E-Mail
+        </label>
+        <div class="col-md-6">
+            <input id="email" type="email" class="form-control"
+                name="email" value="" required>
         </div>
-        <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">
-                Пароль
-            </label>
-            <div class="col-md-6">
-                <input id="password" type="password" class="form-control"
-                    name="password" required>
-            </div>
+    </div>
+    <div class="form-group row">
+        <label for="password" class="col-md-4 col-form-label text-md-right">
+            Пароль
+        </label>
+        <div class="col-md-6">
+            <input id="password" type="password" class="form-control"
+                name="password" required>
         </div>
-        <div class="form-group row">
-            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">
-                Подтвердите пароль
-            </label>
+    </div>
+    <div class="form-group row">
+        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">
+            Подтвердите пароль
+        </label>
         <div class="col-md-6">
             <input id="password-confirm" type="password" class="form-control" 
                 name="password_confirmation" required>
