45b6dab83b58158029127482af7117e4d698b240
Alexander <alexanderxp@ukr.net>
2022-01-09 22:42:01 +0200

    add where('status', 1)->   to OrderController.php

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -10,7 +10,7 @@ class OrderController extends Controller
 {
     public function index()  
     {       
-        $orders = Order::get();             
+        $orders = Order::where('status', 1)->get();             
         dd($orders);
         return view('auth.orders.index');
     }            
