3e07d7ab08f0ac61337f6860b653877f3e8ae695
Alexander <alexanderxp@ukr.net>
2022-01-09 21:59:06 +0200

    add use App\Order;  ,  $orders = Order::get();   ,    dd($orders);   in   OrderController.php

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function index()  
     {       
+        $orders = Order::get();             
+        dd($orders);
         return view('auth.orders.index');
     }            
 }
