65783db111efdab0d6ba3b26169885327180e4a4
Alexander <alexanderxp@ukr.net>
2022-01-11 00:04:53 +0200

    del dd(1); & add $user = auth::user();dd($user);  in  CheckIsAdmin.php

--- app/Http/Middleware/CheckIsAdmin.php
+++ app/Http/Middleware/CheckIsAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class CheckIsAdmin
 {
@@ -15,6 +16,8 @@ class CheckIsAdmin
      */
     public function handle($request, Closure $next)
     {
+        $user = auth::user();    
+        dd($user);                
         return $next($request);
     }
 }
