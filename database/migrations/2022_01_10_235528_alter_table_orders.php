bb2811b70fc4b57b600bff3de311a0c403641de4
Alexander <alexanderxp@ukr.net>
2022-01-11 03:30:44 +0200

    add ->nullable()  in  2022_01_10_235528_alter_table_orders.php

--- database/migrations/2022_01_10_235528_alter_table_orders.php
+++ database/migrations/2022_01_10_235528_alter_table_orders.php
@@ -14,7 +14,7 @@ class AlterTableOrders extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {   
-            $table->Integer('user_id');
+            $table->Integer('user_id')->nullable();
         });
     }
 
