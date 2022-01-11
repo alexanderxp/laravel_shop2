ee5c6e155907c514ab0a6d50fbdea9a93bc1b891
Alexander <alexanderxp@ukr.net>
2022-01-11 10:47:41 +0200

    add use App\Order;   $orderId = session('orderId');if(!is_null($orderId)){$order = Order::findOrFail($orderId);if ($order->products->count() == 0){session()->flash('warning', 'Ваша корзина пуста');return redirect()->route('index');}}  in  BasketIsNotEmpty.php

--- app/Http/Middleware/BasketIsNotEmpty.php
+++ app/Http/Middleware/BasketIsNotEmpty.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Order; 
 use Closure;
 
 class BasketIsNotEmpty
@@ -15,6 +16,15 @@ class BasketIsNotEmpty
      */
     public function handle($request, Closure $next)
     {
+        $orderId = session('orderId');                
+        if(!is_null($orderId)){                   
+            $order = Order::findOrFail($orderId);   
+            if ($order->products->count() == 0){                     
+                session()->flash('warning', 'Ваша корзина пуста'); 
+                return redirect()->route('index');                 
+           }                                                     
+        }                                            
+
         return $next($request);
     }
 }
