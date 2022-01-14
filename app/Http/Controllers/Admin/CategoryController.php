365c5e8cbe41c0c321c6ccfb829fa76b7b7fe7ed
Alexander <alexanderxp@ukr.net>
2022-01-14 17:26:46 +0200

    add $category->update($request->all()); return redirect()->route('categories.index');  in CategoryController.php

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -72,7 +72,8 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $category->update($request->all());    
+        return redirect()->route('categories.index'); 
     }
 
     /**
