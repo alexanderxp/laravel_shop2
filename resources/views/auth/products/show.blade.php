4e6ec8bcab1ea3f1191f6f24f3b2ed24250b11a8
Alexander <alexanderxp@ukr.net>
2022-01-15 20:26:49 +0200

    create new file show.blade.php

---
+++ resources/views/auth/products/show.blade.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="m2SZwWq2bCfl7LFcWk4WN7SXLp1YUTLRv7FcEztt">
+
+    <title>Админка</title>
+
+    <!-- Scripts -->
+    <script src="/js/app.js" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/admin.css" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-default navbar-expand-md navbar-light navbar-laravel">
+        <div class="container">
+            <a class="navbar-brand" href="http://internet-shop.tmweb.ru">
+                Вернуться на сайт
+            </a>
+
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="http://internet-shop.tmweb.ru/admin/categories">Категории</a></li>
+                    <li><a href="http://internet-shop.tmweb.ru/admin/products">Товары</a></li>
+                    <li><a href="http://internet-shop.tmweb.ru/admin/orders">Заказы</a></li>
+                </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="nav-item dropdown">
+                           <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                              aria-haspopup="true" aria-expanded="false" v-pre>
+                                 Администратор
+                           </a>
+                           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="http://internet-shop.tmweb.ru/logout"
+                                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                Выйти
+                              </a>
+                              <form id="logout-form" action="http://internet-shop.tmweb.ru/logout" method="POST" style="display: none">
+                                  <input type="hidden" name="token" value="m2SZwWq2bCfl7LFcWk4WN7SXLp1YUTLRv7FcEztt">
+                              </form>
+                           </div>
+                        </li>
+                     </ul>
+              </div>
+        </div>
+    </nav>
+
+    <div class="py-4"> 
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+
+                    <h1>iPhone X 64GB</h1>
+                    <table class='table'>
+                        <tbody>
+                        <tr>
+                            <th>
+                                Поле
+                            </th>
+                            <th>
+                                Значение
+                            </th>
+                        </tr>
+                        <tr>
+                            <td>ID</td>
+                            <td>1</td>
+                        </tr>
+                        <tr>
+                            <td>Код</td>
+                            <td>iphone_x_64</td>
+                        </tr>
+                        <tr>
+                            <td>Название</td>
+                            <td>iPhone X 64GB</td>
+                        </tr>
+                        <tr>
+                            <td>Описание</td>
+                            <td>Отличный продвинутый телефон с памятью на 64 gb</td>
+                        </tr>
+                        <tr>
+                            <td>Картинка</td>
+                            <td><img src="http://internet-shop.tmweb.ru/storage/products/bosch.jpg" height="240px"></td>
+                        </tr>
+                        <tr>
+                            <td>Категория</td>
+                            <td>Мобильные телефоны</td>
+                        </tr>
+                   </tbody>
+               </div>
+
+            </div>
+         </div>
+     </div>
+</div>
+</body>
+</html>
