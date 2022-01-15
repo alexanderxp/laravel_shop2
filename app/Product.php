844baab0a064b2009efb9d348be0e648edb8de69
Alexander <alexanderxp@ukr.net>
2022-01-15 17:12:01 +0200

    add protected $fillable = ['code', 'name', 'description', 'image', 'price']; in Product.php

--- app/Product.php
+++ app/Product.php
@@ -10,14 +10,17 @@ class Product extends Model
 //    {
 //        return Category::find($this ->category_id); 
 //    }
+
+protected $fillable = ['code', 'name', 'description', 'image', 'price'];
+
     public function category() 
     {
         return $this->belongsTo(Category::class);
     }
     public function getPriceForCount(){          
         if(!is_null($this->pivot)){
-           return $this->pivot->count * $this->price;
-     }
-         return $this->price; 
-     }
+            return $this->pivot->count * $this->price;
+    }
+        return $this->price; 
+    }
 }
