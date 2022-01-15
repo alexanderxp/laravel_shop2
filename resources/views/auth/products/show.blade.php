219b68b50ef868b2321d4d4bf234d19bc94d07f1
Alexander <alexanderxp@ukr.net>
2022-01-15 20:28:46 +0200

    add @extends('auth.layouts.master')  @section('title', 'Продукт ' . $product->name)   @section('content') <div class="col-md-12"><h1>iPhone X 64GB</h1>...@endsection  & del all another HTML code in show.blade.php

--- resources/views/auth/products/show.blade.php
+++ resources/views/auth/products/show.blade.php
@@ -1,66 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('auth.layouts.master')                          
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="m2SZwWq2bCfl7LFcWk4WN7SXLp1YUTLRv7FcEztt">
+@section('title', 'Продукт ' . $product->name)             
 
-    <title>Админка</title>
-
-    <!-- Scripts -->
-    <script src="/js/app.js" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/admin.css" rel="stylesheet">
-</head>
-<body>
-<div id="app">
-    <nav class="navbar navbar-default navbar-expand-md navbar-light navbar-laravel">
-        <div class="container">
-            <a class="navbar-brand" href="http://internet-shop.tmweb.ru">
-                Вернуться на сайт
-            </a>
-
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="http://internet-shop.tmweb.ru/admin/categories">Категории</a></li>
-                    <li><a href="http://internet-shop.tmweb.ru/admin/products">Товары</a></li>
-                    <li><a href="http://internet-shop.tmweb.ru/admin/orders">Заказы</a></li>
-                </ul>
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="nav-item dropdown">
-                           <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
-                              aria-haspopup="true" aria-expanded="false" v-pre>
-                                 Администратор
-                           </a>
-                           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                              <a class="dropdown-item" href="http://internet-shop.tmweb.ru/logout"
-                                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                Выйти
-                              </a>
-                              <form id="logout-form" action="http://internet-shop.tmweb.ru/logout" method="POST" style="display: none">
-                                  <input type="hidden" name="token" value="m2SZwWq2bCfl7LFcWk4WN7SXLp1YUTLRv7FcEztt">
-                              </form>
-                           </div>
-                        </li>
-                     </ul>
-              </div>
-        </div>
-    </nav>
-
-    <div class="py-4"> 
-        <div class="container">
-            <div class="row justify-content-center">
+@section('content')                                                                               
                 <div class="col-md-12">
 
                     <h1>iPhone X 64GB</h1>
@@ -100,10 +42,4 @@
                         </tr>
                    </tbody>
                </div>
-
-            </div>
-         </div>
-     </div>
-</div>
-</body>
-</html>
+@endsection 
