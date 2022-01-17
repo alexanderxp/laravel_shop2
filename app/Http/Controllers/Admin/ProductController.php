2e12d85226c79fb9cfc7f485b664cf4b31b11121
Alexander <alexanderxp@ukr.net>
2022-01-17 19:50:37 +0200

    add use Illuminate\Support\Facades\Storage; to ProductController.php

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Product;
 use App\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
